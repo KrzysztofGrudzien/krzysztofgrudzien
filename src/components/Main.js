1b24d9f59cc270a1afae4f67d62a4ed170b7b72f
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-14 14:34:12 +0200

    feat(component): add additional styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,12 @@ const Main = () => {
                 <h1 className={styles.author__title}>Hi there,<br />
                     I'm Krzysztof </h1>
                 <p className={styles.author__description}>
-                    I’m an aspiring FrontEnd Developer and absolutly crazy about fishing and cooking man. Additional I am intersted UX/UI and web design of general meaning.
+                    I’m an aspiring <b>FrontEnd Developer</b> and absolutly crazy about fishing and cooking man. Additional I am intersted <b>UX/UI</b> and web design of general meaning.
                 </p>
                 <p className={styles.author__description}>
                     You might be interested in checking the list of my skills. Don’t forget to  have a glimpse at my projects. Currently I’m looking for a new challenges so if you have one - please contact with me.
                 </p>
-                <p className={styles.author__email}><a href="mailto:krzysztof.grudzien.fed@gmail.com">krzysztof.grudzien.fed@gmail.com</a></p>
+                <p className={styles.author__email}><a className={styles.author__link} href="mailto:krzysztof.grudzien.fed@gmail.com">krzysztof.grudzien.fed@gmail.com</a></p>
             </section>
             <aside className={styles.image}>
                 <img className={styles.image__decoration} src={iPad} alt="ipad image" />
