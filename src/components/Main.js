98ecccf7a650f4771ad949d54d79c36f6ddaced7
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-14 13:19:49 +0200

    feat(component): add main component and style to it

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../Sass/Components/Main.module.scss'
+const Main = () => {
+    return (
+        <main className={styles.main}>
+            <h1>MAIN</h1>
+        </main>
+    )
+}
+
+export default Main;
