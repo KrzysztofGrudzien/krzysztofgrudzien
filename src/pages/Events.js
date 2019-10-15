d44ff5a85d54ce3803767ab94dc13d16bbf4baaa
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-15 03:31:27 +0200

    feat(router): add routing pages to the app

---
+++ src/pages/Events.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Events = () => {
+    return (
+        <div>Skill</div>
+    )
+}
+
+export default Events;
---
+++ src/pages/ITRoadMap.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ITRoadMap = () => {
+    return (
+        <div>IT RoadMap</div>
+    )
+}
+
+export default ITRoadMap;
