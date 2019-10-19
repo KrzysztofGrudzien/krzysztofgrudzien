92cbf5f5ed86a1b0e1baf51d2c68a9a15ee782e2
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-20 01:06:50 +0200

    Updates

---
+++ precache-manifest.3c1587320d72f9868a3ae991c83043f8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c692685fe1ffe757856b5c4b18d601b0",
+    "url": "/krzysztofgrudzien/index.html"
+  },
+  {
+    "revision": "4bf7e98dcf2fbc9f15b2",
+    "url": "/krzysztofgrudzien/static/css/main.bb27e2aa.chunk.css"
+  },
+  {
+    "revision": "db1776f248b142e60954",
+    "url": "/krzysztofgrudzien/static/js/2.5d71ec0b.chunk.js"
+  },
+  {
+    "revision": "4bf7e98dcf2fbc9f15b2",
+    "url": "/krzysztofgrudzien/static/js/main.162841d5.chunk.js"
+  },
+  {
+    "revision": "307060ac2a8644c1ef1c",
+    "url": "/krzysztofgrudzien/static/js/runtime-main.388e153c.js"
+  },
+  {
+    "revision": "906133cfa1c35b158b42348368e167d5",
+    "url": "/krzysztofgrudzien/static/media/author.906133cf.png"
+  },
+  {
+    "revision": "d1299555016c1f4e6c24960ef4a4a134",
+    "url": "/krzysztofgrudzien/static/media/check-blue.d1299555.svg"
+  },
+  {
+    "revision": "617341d397d86e05931845ebe49a29cb",
+    "url": "/krzysztofgrudzien/static/media/check-green.617341d3.svg"
+  },
+  {
+    "revision": "5307b738026615b711dc2e4b77c47e6f",
+    "url": "/krzysztofgrudzien/static/media/check-violet.5307b738.svg"
+  },
+  {
+    "revision": "2034ac9f962ad792522b0138a6f7fa27",
+    "url": "/krzysztofgrudzien/static/media/check-yellow.2034ac9f.svg"
+  },
+  {
+    "revision": "a829a7d2a550efd5f5669dd4f91309ae",
+    "url": "/krzysztofgrudzien/static/media/code-icon.a829a7d2.svg"
+  },
+  {
+    "revision": "ed087f1dd25cb9d2d7fa78fdb0bd15b2",
+    "url": "/krzysztofgrudzien/static/media/contact.ed087f1d.jpg"
+  },
+  {
+    "revision": "294869ba7be25404db8fcbb8ead70698",
+    "url": "/krzysztofgrudzien/static/media/criscode.294869ba.jpg"
+  },
+  {
+    "revision": "59e3445b6578fdcb23b32f01a2f5674d",
+    "url": "/krzysztofgrudzien/static/media/criscode.59e3445b.svg"
+  },
+  {
+    "revision": "65d723016603da6947b4b018dd5a8e2e",
+    "url": "/krzysztofgrudzien/static/media/deer.65d72301.jpg"
+  },
+  {
+    "revision": "41690849a1c744418e825f925323f4f7",
+    "url": "/krzysztofgrudzien/static/media/demo-icon.41690849.svg"
+  },
+  {
+    "revision": "3bc4a79cfa169ebab65cbe52ef646173",
+    "url": "/krzysztofgrudzien/static/media/events.3bc4a79c.jpg"
+  },
+  {
+    "revision": "d5c188236a3761e60fe93eeda090878d",
+    "url": "/krzysztofgrudzien/static/media/facebook.d5c18823.svg"
+  },
+  {
+    "revision": "9ff24124305c5f4691e127964f169fa7",
+    "url": "/krzysztofgrudzien/static/media/github.9ff24124.svg"
+  },
+  {
+    "revision": "704ae428d2236b7deef98dd5d5d32808",
+    "url": "/krzysztofgrudzien/static/media/hydrapp.704ae428.jpg"
+  },
+  {
+    "revision": "825fbe0039e0001a0c4177d4b1aa175c",
+    "url": "/krzysztofgrudzien/static/media/lab.825fbe00.jpg"
+  },
+  {
+    "revision": "1d86e2ae19c605d23441189f5445a05c",
+    "url": "/krzysztofgrudzien/static/media/linkedin.1d86e2ae.svg"
+  },
+  {
+    "revision": "838a992156a92e7c397628e54eaa02a5",
+    "url": "/krzysztofgrudzien/static/media/portrait2x.838a9921.png"
+  },
+  {
+    "revision": "71ff58c6f2699c8e8e54f34d7acbfafb",
+    "url": "/krzysztofgrudzien/static/media/programming.71ff58c6.jpg"
+  },
+  {
+    "revision": "73ded10296190178a44f8a569bcd59cf",
+    "url": "/krzysztofgrudzien/static/media/road.73ded102.jpg"
+  },
+  {
+    "revision": "9bceb3eaf3a66f4815f67d3bff8c4cce",
+    "url": "/krzysztofgrudzien/static/media/wolf.9bceb3ea.jpg"
+  },
+  {
+    "revision": "d3f40b71f56aeb4a12e969004e53127a",
+    "url": "/krzysztofgrudzien/static/media/wtfteam@2x.d3f40b71.jpg"
+  }
+]);
