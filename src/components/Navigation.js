a532e76cc9e98fbd6e276e1abe6457079c4053bc
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-22 11:35:42 +0200

    refactor(component): refactore code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,48 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from '../Sass/Components/Navigation.module.scss';
+
+const listLinks = [
+    { number: "01", path: "/", name: "about", prefix: "me" },
+    { number: "02", path: "/skills", name: "skills", prefix: "current" },
+    { number: "03", path: "/roadmap", name: "my it", prefix: "roadmap" },
+    { number: "04", path: "/works", name: "works", prefix: "best" },
+    { number: "05", path: "/lab", name: "lab", prefix: "my" },
+    { number: "06", path: "/events", name: "event", prefix: "'s" },
+    { number: "07", path: "/contact", name: "contact", prefix: "with me" }
+]
 const Menu = () => {
+    const links = listLinks.map(link => (
+        <li key={link.number} className={styles.nav__item}>
+            <sup className={styles.nav__number}>{link.number}</sup>
+            <NavLink to={link.path} className={styles.nav__link}>{link.name}</NavLink>
+            <sup className={styles.nav__linkDescription}>{link.prefix}</sup>
+        </li>
+    ))
     return (
         <>
             <div className={styles.switcher}>
                 <span className={styles.switcher__text}>MENU</span>
             </div>
-            <nav className={styles.nav} hidden>
+            <nav className={styles.nav}>
                 <ul className={styles.nav__list}>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>01</sup>
-                        <a href="#" className={styles.nav__link}>About</a>
-                        <sup className={styles.nav__linkDescription}>me</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>02</sup>
-                        <a href="#" className={styles.nav__link}>Skills</a>
-                        <sup className={styles.nav__linkDescription}>current</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>03</sup>
-                        <a href="#" className={styles.nav__link}>My It</a>
-                        <sup className={styles.nav__linkDescription}>roadmap</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>04</sup>
-                        <a href="#" className={styles.nav__link}>Works</a>
-                        <sup className={styles.nav__linkDescription}>best</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>05</sup>
-                        <a href="#" className={styles.nav__link}>lab</a>
-                        <sup className={styles.nav__linkDescription}>may</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>06</sup>
-                        <a href="#" className={styles.nav__link}>Event</a>
-                        <sup className={styles.nav__linkDescription}>'s</sup>
-                    </li>
-                    <li className={styles.nav__item}>
-                        <sup className={styles.nav__number}>07</sup>
-                        <a href="#" className={styles.nav__link}>Contact</a>
-                        <sup className={styles.nav__linkDescription}>with me</sup>
-                    </li>
+                    {links}
                 </ul>
             </nav>
         </>
