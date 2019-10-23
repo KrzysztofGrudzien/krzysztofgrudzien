8cbd6343238c5707774a920a6fd796cd9d8c9478
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-24 00:52:57 +0200

    fix(component): change structure of component

--- src/pages/ITRoadMap.js
+++ src/pages/ITRoadMap.js
@@ -6,30 +6,74 @@ const ITRoadMap = () => {
             <section className={styles.roadmap}>
                 <div className={styles.roadmap__hero}></div>
                 <h1 className={styles.roadmap__title}>IT roadmap</h1>
-                <div className={styles.roadmap__wrapper}>
-                    <div className={styles.part__number}>
-                        <span className={styles.part__numberText}>2018</span>
-                    </div>
-                    <section className={styles.part}>
-                        <h2 className={styles.part__title}>Start programming</h2>
-                        <p className={styles.part__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, maxime? Aut, iste. Iste atque in omnis fugiat maxime cupiditate perspiciatis voluptas voluptatem, quod tenetur et! Rerum quae alias libero labore!</p>
-                    </section>
-                    <section className={styles.part}>
-                        <h2 className={styles.part__title}>WTF Course</h2>
-                        <p className={styles.part__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, maxime? Aut, iste. Iste atque in omnis fugiat maxime cupiditate perspiciatis voluptas voluptatem, quod tenetur et! Rerum quae alias libero labore!</p>
-                    </section>
-                    <div className={styles.part__number}>
-                        <span className={styles.part__numberText}>2020</span>
-                    </div>
-                    <section className={styles.part}>
-                        <h2 className={styles.part__title}>React Course</h2>
-                        <p className={styles.part__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, maxime? Aut, iste. Iste atque in omnis fugiat maxime cupiditate perspiciatis voluptas voluptatem, quod tenetur et! Rerum quae alias libero labore!</p>
-                    </section>
+                <div className={styles.timeline}>
+                    <ul className={styles.timeline__list}>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <li className={styles.timeline__item}>
+                            <article className={styles.article}>
+                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
+                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                            </article>
+                            <div className={styles.time}>
+                                <h3 className={styles.time__title}>January 2018</h3>
+                            </div>
+                        </li>
+                        <div className={styles.timeline__clearFix}></div>
+                    </ul>
                 </div>
-                <article className={styles.article}>
-                    <h2 className={styles.article__title}>Why frontend?</h2>
-                    <p className={styles.article__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis modi vero consectetur tempore deleniti quod! Nulla assumenda illo nobis nihil? Blanditiis illo eum minima molestiae rerum consequuntur possimus nemo magnam!</p>
-                </article>
             </section>
         </main>
     )
