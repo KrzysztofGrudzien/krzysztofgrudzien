8e9399309106da35f06a833bf1ab7f6ced70cfc1
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-25 23:36:36 +0200

    fix(component): remove old and add new structure of it path

--- src/pages/ITRoadMap.js
+++ src/pages/ITRoadMap.js
@@ -10,65 +10,74 @@ const ITRoadMap = () => {
                     <ul className={styles.timeline__list}>
                         <li className={styles.timeline__item}>
                             <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                                <h2 className={styles.article__title}>Hobby</h2>
+                                <p className={styles.article__description}>Here, I didn't suppose I want to be a programmer. I've started learning HTML, CSS, JS, PHP, MYSQL, ACTION SCRIPT, JAVA, and many different languages. I didn't mention I've started using graphics programs like Adobe CS, Gimp etc, but ... . Many times I had to stop my learning and started again. My personal life prevented continouse learning. In short, the main thing I've learnt for those years - don't give up and don't use many languages and tools before you master the next one.</p>
                             </article>
                             <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
+                                <h3 className={styles.time__title}>to summer 2017</h3>
                             </div>
                         </li>
                         <li className={styles.timeline__item}>
                             <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                                <h2 className={styles.article__title}>FrontEnd or BACKEND</h2>
+                                <p className={styles.article__description}>I thought about it a lot. I've started learning JAVA and HTML, CSS, JS in the same time. The choose was very easy. Backend is not for me. I have to see my works in action. I love design and interaction. My projects have to live and people seeing it have to say - o f... It's look great. That's way I've chosen frontend. I've started using many courses from EDUWEB.PL and UDEMY.COM. because only what I did before it was only watching and collecting them on my disk.</p>
+                                <h3 className={styles.article__titleList}>Chosen topics</h3>
+                                <ul className={styles.article__list}>
+                                    <li className={styles.article__item}>Learning BEM</li>
+                                    <li className={styles.article__item}>How Flex Box and Grid works</li>
+                                    <li className={styles.article__item}>Basic concept of JS</li>
+                                    <li className={styles.article__item}>What is it GIT</li>
+                                    <li className={styles.article__item}>Bootstrap and other frameworks</li>
+                                    <li className={styles.article__item}>Adobe CC - PS, Ai</li>
+                                    <li className={styles.article__item}>UX/UI</li>
+                                    <li className={styles.article__item}>HTML5, CSS3, SASS</li>
+                                    <li className={styles.article__item}>Task Runners</li>
+                                </ul>
                             </article>
                             <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
+                                <h3 className={styles.time__title}>to summer 2018</h3>
                             </div>
                         </li>
                         <li className={styles.timeline__item}>
                             <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                                <h2 className={styles.article__title}>WTF: What the frontend</h2>
+                                <p className={styles.article__description}>It's a 12-week practical course learning beginner adept of programming many useful skills about frontend knowledge. If you want to know how the knowlegde path looks like <a href="https://cotenfrontend.pl/#program-szkolenia" className={styles.article__link} target="_blank"> click this link</a>
+                                </p>
+                                <h3 className={styles.article__titleList}>Chosen topics</h3>
+                                <ul className={styles.article__list}>
+                                    <li className={styles.article__item}>BEM</li>
+                                    <li className={styles.article__item}>Flex Box and Grid</li>
+                                    <li className={styles.article__item}>JSON and REST API</li>
+                                    <li className={styles.article__item}>CSS animations and SVG</li>
+                                    <li className={styles.article__item}>GIT, PWA, WCAG, OpenGraph</li>
+                                    <li className={styles.article__item}>Node.js, NPM, GULP</li>
+                                    <li className={styles.article__item}>Deploy</li>
+                                </ul>
                             </article>
                             <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
+                                <h3 className={styles.time__title}>May 2019</h3>
                             </div>
                         </li>
                         <li className={styles.timeline__item}>
                             <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
+                                <h2 className={styles.article__title}>React Course</h2>
+                                <p className={styles.article__description}>It's a 12-week practical course learning much more advanced adept of programming. If you want to know how the knowlegde path looks like <a href="https://kursreacta.pl/" className={styles.article__link} target="_blank"> click this link</a></p>
+                                <h3 className={styles.article__titleList}>Chosen topics</h3>
+                                <ul className={styles.article__list}>
+                                    <li className={styles.article__item}>Components</li>
+                                    <li className={styles.article__item}>Forms and styles</li>
+                                    <li className={styles.article__item}>Create React App</li>
+                                    <li className={styles.article__item}>Testing App - JEST, TDD</li>
+                                    <li className={styles.article__item}>Library integration</li>
+                                    <li className={styles.article__item}>REST API, Authorization</li>
+                                    <li className={styles.article__item}>Hooks</li>
+                                    <li className={styles.article__item}>Advance Patterns</li>
+                                    <li className={styles.article__item}>Redux</li>
+                                    <li className={styles.article__item}>Deploy App</li>
+                                </ul>
                             </article>
                             <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
-                            </div>
-                        </li>
-                        <li className={styles.timeline__item}>
-                            <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
-                            </article>
-                            <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
-                            </div>
-                        </li>
-                        <li className={styles.timeline__item}>
-                            <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
-                            </article>
-                            <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
-                            </div>
-                        </li>
-                        <li className={styles.timeline__item}>
-                            <article className={styles.article}>
-                                <h2 className={styles.article__title}>WTF - What the FrontEnd ?</h2>
-                                <p className={styles.article__description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nobis cum officia ad reiciendis tempora. Velit, amet ut laborum quia, totam similique modi, quasi sint adipisci praesentium quam ipsum. Harum?</p>
-                            </article>
-                            <div className={styles.time}>
-                                <h3 className={styles.time__title}>January 2018</h3>
+                                <h3 className={styles.time__title}>January 2019</h3>
                             </div>
                         </li>
                         <div className={styles.timeline__clearFix}></div>
