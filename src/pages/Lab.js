cedf6e802a14c7a7227f96bb1719492017747a11
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-10-30 22:25:50 +0100

    feat(scss): add RWD styles for contact page

--- src/pages/Lab.js
+++ src/pages/Lab.js
@@ -12,6 +12,27 @@ const Lab = () => {
                 <h1 className={styles.lab__title}>Laboratory</h1>
                 <div className={styles.lab__wrapper}>
                     <article className={styles.article}>
+                        <h2 className={styles.article__title}>remodeling ...</h2>
+                        {/* <img className={styles.article__image} src={hydrApp} alt="" />
+                        <ul className={styles.article__list}>
+                            <li className={styles.article__item}><b>Role:</b> Design, FrontEnd </li>
+                            <li className={styles.article__item}><b>Category:</b> PWA </li>
+                            <li className={styles.article__item}><b>Data:</b> 2019-09-09 </li>
+                            <li className={styles.article__item}><b>Technology:</b> CSS3, HTML5, JS</li>
+                        </ul>
+                        <p className={styles.article__description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam tenetur sit in mollitia quisquam temporibus possimus nihil veritatis quod nesciunt, nisi quasi deserunt quos voluptate dolor earum sint veniam maxime?</p>
+                        <footer className={styles.article__footer}>
+                            <div className={styles.article__demo}>
+                                <img className={styles.article__icon} src={iconDemo} alt="icon preview." />
+                                <a href="#" className={styles.article__link}>Launch</a>
+                            </div>
+                            <div className={styles.article__code}>
+                                <img className={styles.article__icon} src={iconCode} alt="icon code." />
+                                <a href="#" className={styles.article__link}>GitHub</a>
+                            </div>
+                        </footer> */}
+                    </article>
+                    {/* <article className={styles.article}>
                         <h2 className={styles.article__title}>hydrAPP - PWA APP</h2>
                         <img className={styles.article__image} src={hydrApp} alt="" />
                         <ul className={styles.article__list}>
@@ -31,28 +52,7 @@ const Lab = () => {
                                 <a href="#" className={styles.article__link}>GitHub</a>
                             </div>
                         </footer>
-                    </article>
-                    <article className={styles.article}>
-                        <h2 className={styles.article__title}>hydrAPP - PWA APP</h2>
-                        <img className={styles.article__image} src={hydrApp} alt="" />
-                        <ul className={styles.article__list}>
-                            <li className={styles.article__item}><b>Role:</b> Design, FrontEnd </li>
-                            <li className={styles.article__item}><b>Category:</b> PWA </li>
-                            <li className={styles.article__item}><b>Data:</b> 2019-09-09 </li>
-                            <li className={styles.article__item}><b>Technology:</b> CSS3, HTML5, JS</li>
-                        </ul>
-                        <p className={styles.article__description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam tenetur sit in mollitia quisquam temporibus possimus nihil veritatis quod nesciunt, nisi quasi deserunt quos voluptate dolor earum sint veniam maxime?</p>
-                        <footer className={styles.article__footer}>
-                            <div className={styles.article__demo}>
-                                <img className={styles.article__icon} src={iconDemo} alt="icon preview." />
-                                <a href="#" className={styles.article__link}>Launch</a>
-                            </div>
-                            <div className={styles.article__code}>
-                                <img className={styles.article__icon} src={iconCode} alt="icon code." />
-                                <a href="#" className={styles.article__link}>GitHub</a>
-                            </div>
-                        </footer>
-                    </article>
+                    </article> */}
                 </div>
             </section>
         </main>
